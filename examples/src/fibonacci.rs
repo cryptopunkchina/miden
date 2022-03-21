0b869ba84b68b2425ffb4cb699c29cfaa0d9a468
yaanhyy <78102887@qq.com>
2022-03-21 21:26:28 +0800

    test

--- examples/src/fibonacci.rs
+++ examples/src/fibonacci.rs
@@ -62,7 +62,7 @@ fn compute_fibonacci(n: usize) -> Felt {
 
 #[test]
 fn test_fib_example() {
-    let example = get_example(16);
+    let example = get_example(16000);
     super::test_example(example, false);
 }
 
